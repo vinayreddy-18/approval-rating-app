acf406663ec374d6057ace26af1bee1f694e3959
vinayreddy-18 <vinayreddysathi2007@gmail.com>
2026-06-22 10:38:26 +0530

    fix postgres login insert

--- run.py
+++ run.py
@@ -1,10 +1,6 @@
 from app import create_app
-from app.database import init_db, seed_politicians
-
-init_db()
-seed_politicians()
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
